42b5eb540317dc9d2fd05e4d3a17d764a561579b
Ruslan <fynex@mail.ru>
2024-12-23 14:37:35 +0300

    add mask

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import os
 import torch
@@ -41,6 +42,26 @@ def postprocess_output(output):
     output = torch.argmax(output, dim=1).squeeze(0).cpu().numpy()
     return output.astype(np.uint8)
 
+
+# Наложение маски на изображение
+def overlay_mask(image: np.ndarray, mask: np.ndarray, alpha=0.6):
+    """
+    Наложение маски на изображение.
+    image: исходное изображение (H, W, 3)
+    mask: предсказанная маска (H, W)
+    alpha: прозрачность маски
+    """
+    # Приведение маски к размеру изображения
+    resized_mask = cv2.resize(mask, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_NEAREST)
+    
+    # Создание цветной маски
+    colored_mask = np.zeros_like(image)
+    colored_mask[..., 1] = (resized_mask * 255).astype(np.uint8)  # Зеленый цвет для маски
+    
+    # Наложение маски
+    overlay = ((1 - alpha) * image + alpha * colored_mask).astype(np.uint8)
+    return overlay
+
 # Эндпоинт для инференса
 @app.post("/segment")
 async def segment_image(file: UploadFile = File(...)):
@@ -48,6 +69,7 @@ async def segment_image(file: UploadFile = File(...)):
         # Открытие изображения
         image_bytes = await file.read()
         image = Image.open(BytesIO(image_bytes)).convert("RGB")
+        original_image = np.array(image)
         
         # Предобработка
         input_tensor = preprocess_image(image)
@@ -57,10 +79,12 @@ async def segment_image(file: UploadFile = File(...)):
             output = model(input_tensor)
         
         # Постобработка
-        segmented_image = postprocess_output(output)
+        segmented_mask  = postprocess_output(output)
+
+        overlaid_image = overlay_mask(original_image, segmented_mask)
         
         # Конвертация результата в Base64 или сохранение
-        segmented_pil_image = Image.fromarray(segmented_image)
+        segmented_pil_image = Image.fromarray(overlaid_image)
         saved_dir = os.getcwd() + "/results/"
         save_path = os.path.join(saved_dir, f"segmented_{file.filename}")
         segmented_pil_image.save(save_path, format="PNG")
